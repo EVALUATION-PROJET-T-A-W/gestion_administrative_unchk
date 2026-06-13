1ffbb26b795960fc42aeedaa20041204549b0e7b
Anna Marone <annamarone72@gmail.com>
2026-06-13 14:08:27 +0000

    implementation des fontionnalite du profile de l'etudiant et formateur connecter

--- backend/src/main/java/sn/uchk/universite/repository/EmploiDuTempsRepository.java
+++ backend/src/main/java/sn/uchk/universite/repository/EmploiDuTempsRepository.java
@@ -3,5 +3,8 @@ package sn.uchk.universite.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sn.uchk.universite.entity.EmploiDuTemps;
 
+import java.util.List;
+
 public interface EmploiDuTempsRepository extends JpaRepository<EmploiDuTemps, Long> {
+     List<EmploiDuTemps> findByFormationId(Long formationId);
 }
